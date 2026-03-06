88272843638a8ba4da2f68466580539360335556
MauriceElliott <mauricemelliott@gmail.com>
2026-03-06 15:23:36 +0000

    almost there on the navbar fix

--- Sources/Components/MainNavBar.swift
+++ Sources/Components/MainNavBar.swift
@@ -1,40 +1,19 @@
 import Foundation
 import Ignite
 
-enum NavOption: String, CaseIterable {
-    case about = "About"
-    case contact = "Contact"
-    case events = "Events"
-    case media = "Media"
-    case ministries = "Ministries"
-    
-    @MainActor var path: any StaticPage {
-        switch self {
-        case .about:
-            return About()
-        case .contact:
-            return Contact()
-        case .events:
-            return Events()
-        case .media:
-            return Media()
-        case .ministries:
-            return Ministries()
-        }
-    }
-}
-
 struct MainNavBar: HTML {
     var body: some HTML {
         NavigationBar(logo: logo) {
-            for option in NavOption.allCases {
-                Link(option.rawValue, target: option.path)
-            }
+            Link("About", target: About())
+            Link("Contact", target: Contact())
+            Link("Events", target: Events())
+            Link("Media", target: Media())
+            Link("Ministries", target: Ministries())
         }
         .navigationItemAlignment(.trailing)
         .navigationBarStyle(.light)
         .position(.fixedTop)
-        .background(Color(hex: "#F2DFDA"))
+        .background(Color(hex: "#F2BFB1"))
     }
     
     private var logo: some InlineElement {
