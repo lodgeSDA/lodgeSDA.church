dc39cf3b46253832301e6a0c326d765a4842aea8
MauriceElliott <mauricemelliott@gmail.com>
2026-03-16 22:22:55 +0000

    fixup! fixup! fixup! Updated the about page

--- Sources/Pages/About.swift
+++ Sources/Pages/About.swift
@@ -6,10 +6,11 @@ struct About: StaticPage {
 
     var body: some HTML {
         Section {
-            Text(markdown: """
-                ### Mission
-                    As Seventh-day Adventists, our mission drives everything we do. Rooted in Christ’s love and the teachings of the Bible, we are committed to serving others, transforming lives, and building communities of hope. Through education, humanitarian work, health initiatives, and spiritual guidance, we aim to inspire faith and create lasting impact in the lives we touch—both locally and globally.
-            """)
+            Text(Strong("Mission")).font(.title1)
+            
+                   
+            Text("As Seventh-day Adventists, our mission drives everything we do. Rooted in Christ’s love and the teachings of the Bible, we are committed to serving others, transforming lives, and building communities of hope. Through education, humanitarian work, health initiatives, and spiritual guidance, we aim to inspire faith and create lasting impact in the lives we touch—both locally and globally.").font(.body)
+            
             Link("Tithes and Donations", target: "https://sec.adventist.uk/giving/tithes-offerings-and-donations")
                 .linkStyle(.button)
 
